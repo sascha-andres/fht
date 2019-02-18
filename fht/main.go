51550788d468f8bf775b0520a66b1a457e86815f
Sascha Andres <sascha.andres@outlook.com>
2019-02-18 10:38:04 +0100

    feat: use parser in main

--- fht/main.go
+++ fht/main.go
@@ -1,1 +1,33 @@
-package fht
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"livingit.de/code/fht/parser"
+	"os"
+)
+
+func main() {
+	stat, _ := os.Stdin.Stat()
+	if !((stat.Mode() & os.ModeCharDevice) == 0) {
+		_, _ = fmt.Fprintln(os.Stderr, "expecting fish history to be piped")
+		os.Exit(1)
+	}
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error reading piped data: %s\n", err)
+		os.Exit(1)
+	}
+
+	p, err := parser.NewParser()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error creating parser: %s\n", err)
+		os.Exit(1)
+	}
+
+	_, err = p.ParseString(string(data))
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error parsing: %s\n", err)
+		os.Exit(1)
+	}
+}
