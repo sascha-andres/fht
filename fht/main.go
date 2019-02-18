ef437adcf364c947819b933994e067225ab2b68f
Sascha Andres <sascha.andres@outlook.com>
2019-02-18 12:55:45 +0100

    feat: print history

--- fht/main.go
+++ fht/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"livingit.de/code/fht/parser"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -25,9 +26,56 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = p.ParseString(string(data))
+	history, err := p.ParseString(string(data))
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error parsing: %s\n", err)
 		os.Exit(1)
+	}
+
+	showHelp := false
+	delete := false
+	expression := ""
+
+	arguments := os.Args
+	for _, argument := range arguments[1:] {
+		switch argument {
+		case "--help":
+			showHelp = true
+		case "--delete":
+			delete = true
+		default:
+			if "" == expression {
+				expression = argument
+			} else {
+				_, _ = fmt.Fprintln(os.Stderr, "only one expression allowed")
+				os.Exit(1)
+			}
+		}
+	}
+
+	if !showHelp && expression == "" {
+		showHelp = true
+	}
+
+	if showHelp {
+		fmt.Println("usage fht [--delete] <expression>")
+		os.Exit(0)
+	}
+
+	r, err := regexp.Compile(expression)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error compiling regex: %s", err)
+		os.Exit(1)
+	}
+
+	for _, entry := range history {
+		isMatch, _ := entry.Matches(r)
+		if isMatch && delete {
+			continue
+		}
+		if !isMatch && !delete {
+			continue
+		}
+		_, _ = fmt.Fprintln(os.Stdout, &entry)
 	}
 }
